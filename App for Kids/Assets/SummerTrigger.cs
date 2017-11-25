634defa336145140d8f0880f34839e01955c6274
martvanochten <martvanochten@gmail.com>
2017-11-25 11:46:14 +0100

    Borders
    test not done yet

---
+++ App for Kids/Assets/SummerTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SummerTrigger : MonoBehaviour {
+
+    public GameObject zeroLayer;
+    public GameObject leftBorder;
+    public GameObject rightBorder;
+    public float timer;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        timer = 0f;
+    }
+
+    void Update () {
+
+        float x = zeroLayer.transform.position.x;
+        float tresholdL = leftBorder.transform.position.x;
+        float tresholdR = rightBorder.transform.position.x;
+
+        if (x > tresholdL && x < tresholdR)
+        {
+            Debug.Log("hoi");
+        } else
+        {
+
+        }
+	}
+}
