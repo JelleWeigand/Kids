256a748261e3a414fb95a8708868b0a6bd523c80
martvanochten <martvanochten@gmail.com>
2017-11-25 12:15:36 +0100

    border and timer
    borders and timer

--- App for Kids/Assets/SummerTrigger.cs
+++ App for Kids/Assets/SummerTrigger.cs
@@ -7,26 +7,66 @@ public class SummerTrigger : MonoBehaviour {
     public GameObject zeroLayer;
     public GameObject leftBorder;
     public GameObject rightBorder;
-    public float timer;
+    public float resetTimeSec;
+
+    private float timer;
+    private bool insideSeason;
+    private bool isTriggered = false;
+    private float x;
+    private float tresholdL;
+    private float tresholdR;
 
     // Update is called once per frame
     private void Start()
     {
         timer = 0f;
+        insideSeason = false;
     }
 
     void Update () {
 
-        float x = zeroLayer.transform.position.x;
-        float tresholdL = leftBorder.transform.position.x;
-        float tresholdR = rightBorder.transform.position.x;
-
+        x = zeroLayer.transform.position.x;
+        tresholdL = leftBorder.transform.position.x;
+        tresholdR = rightBorder.transform.position.x;
+        // inside Season
         if (x > tresholdL && x < tresholdR)
         {
-            Debug.Log("hoi");
+            insideSeason = true;
         } else
         {
-
+            insideSeason = false;
+            isTriggered = false;
         }
-	}
+        // triggers all the triggers that only happen once when you're in a season
+        // when you go out and in it triggers again after reset time
+        if (insideSeason && timer <= 0 && !isTriggered)
+        {
+            timer = resetTimeSec;
+            SeasonTriggerUnique();
+            isTriggered = true;
+        }
+        // triggers all the triggers that happen every resetTime
+        if (insideSeason && timer <= 0)
+        {
+            timer = resetTimeSec;
+            SeasonTrigger();
+            isTriggered = true;
+        }
+
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        
+    }
+
+    private void SeasonTriggerUnique ()
+    {
+        Debug.Log("hoi");
+    }
+
+    private void SeasonTrigger()
+    {
+        Debug.Log("hoi");
+    }
 }
