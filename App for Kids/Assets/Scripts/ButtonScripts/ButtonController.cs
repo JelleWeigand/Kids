b408a28dd3321c1ee21c9e364a067f22c7db8ece
JelleWeigand <jelle_weigand@hotmail.com>
2017-11-26 11:03:50 +0100

    Button

---
+++ App for Kids/Assets/Scripts/ButtonScripts/ButtonController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour {
+    //private variables
+    private bool play = false;
+    private float start;
+
+    //public variables
+    public float rotationSpeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+
+    void OnMouseUpAsButton() {
+        if(TouchControl.touchTap&&!play) {
+            play = true;
+            start = Time.time;
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+		if(play) {
+            transform.Rotate(new Vector3(0,0,360*Time.deltaTime /rotationSpeed));
+            if(Time.time > start + rotationSpeed) {
+                play = false;
+            }
+        }
+	}
+}
---
+++ App for Kids/Assets/Scripts/SunScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunScript : MonoBehaviour {
+    //private variables
+    private bool play = false;
+    private bool up = true;
+    private float left;
+    private float right;
+    private float pos;
+    private GameObject  position;
+    private float fac;
+
+    //public variables
+    public float curve;
+    public float height;
+    public float bump1;
+    public float bump2;
+	// Use this for initialization
+	void Start () {
+        left = GameObject.Find("BorderLeft").transform.position.x;
+        right = GameObject.Find("BorderRight").transform.position.x;
+        position = GameObject.Find("PositionObject");
+        fac = bump1 / Mathf.Pow((right-left)/2,curve);
+    }
+
+    void OnMouseUpAsButton() {
+        if(TouchControl.touchTap&&!play) {
+            play = true;
+            up = !up;
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if(up) {
+            
+        }
+    }
+}
