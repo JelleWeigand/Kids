156e29ee31195fba448553a1fa764f9ad30029ed
JelleWeigand <jelle_weigand@hotmail.com>
2017-11-26 15:16:37 +0100

    Sun Update

--- App for Kids/Assets/Scripts/SunScript.cs
+++ App for Kids/Assets/Scripts/SunScript.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class SunScript : MonoBehaviour {
     //private variables
-    private bool play = false;
-    private bool up = true;
     private float left;
     private float right;
     private float x;
-    private float start;
     private GameObject  position;
     private float fac;
     private float v;
+    private float vend;
     private float leap = 0f;
+    private float initialLeap;
 
     //public variables
     public float curve;
@@ -22,38 +21,49 @@ public class SunScript : MonoBehaviour {
     public float bump2;
     public float tranTime;
     public float gravity;
-	// Use this for initialization
-	void Start () {
+    public static bool night = false;
+    public static bool playSun = false;
+    public static float startTimeSun;
+    public bool sun;
+    // Use this for initialization
+    void Start () {
         left = GameObject.Find("BorderLeft").transform.position.x;
         right = GameObject.Find("BorderRight").transform.position.x;
         position = GameObject.Find("PositionObject");
         fac = bump1 / Mathf.Pow((right-left)/2,curve);
         v = Mathf.Pow(gravity*bump2*2,0.5f);
+        vend = -v+gravity*tranTime;
+        initialLeap = v*tranTime-0.5f*gravity*tranTime*tranTime;
     }
 
     void OnMouseUpAsButton() {
-        if(TouchControl.touchTap&&!play) {
-            play = true;
-            up = !up;
-            start = Time.time;
+        if(TouchControl.touchTap&&!playSun) {
+            playSun = true;
+            night = !night;
+            startTimeSun = Time.time;
         }
     }
     
     // Update is called once per frame
     void Update () {
-        if(play) {
-            leap = (Time.time-start)*v-0.5f*gravity*Mathf.Pow((Time.time-start),2f);
-            if (Time.time-start > tranTime) {
-                play = false;
-                if(up) {
-                    leap = 0;
+        Debug.Log(playSun);
+        if(playSun) {
+            if(night ^ sun) {
+                leap = (Time.time-startTimeSun)*v-0.5f*gravity*Mathf.Pow((Time.time-startTimeSun),2f);
+                if(Time.time-startTimeSun > tranTime) {
+                    playSun = false;
+                    leap = initialLeap;
                 }
-                else {
-                    leap = -100;
+            }
+            else {
+                leap = (Time.time-startTimeSun)*vend-0.5f*gravity*Mathf.Pow((Time.time-startTimeSun),2f)+initialLeap;
+                if(Time.time-startTimeSun > tranTime) {
+                    playSun = false;
+                    leap = 0;
                 }
             }
         }
-        if(up||play) {
+        if((night^sun)||playSun) {
             x = position.transform.position.x;
             Vector3 pos = transform.position;
             pos[1] = height + bump1 - Mathf.Clamp(fac*Mathf.Pow((x-(right+left)/2),curve),0,bump1) + leap;
