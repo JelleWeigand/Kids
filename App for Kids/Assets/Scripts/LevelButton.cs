4c0347c4645a7587c7d6b58a53346c92578317e8
martvanochten <martvanochten@gmail.com>
2017-12-04 22:08:13 +0100

    Levelbutton

---
+++ App for Kids/Assets/Scripts/LevelButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseDown()
+    {
+        
+    }
+
+    void OnMouseUpAsButton()
+    {
+        if (TouchControl.touchTap)
+        {
+            Debug.Log("HALLO MART!");
+            Application.LoadLevel("MagneticLeaves");
+        }
+    }
+}
